db493ba2310a790656aa22ebc544c8defd9bedec
John Arbash Meinel <john@arbash-meinel.com>
2009-04-27 16:41:20 -0500

    Add a 'strip_duplicates.py' script. This is equivalent to 'sort -u', only we don't have to actually sort anything.

---
+++ strip_duplicates.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+"""Remove duplicated object information.
+
+To be memory efficient, 'scanner.dump_gc_objects()' does not track what objects
+have already been dumped. This means that it occasionally dumps the same object
+multiple times.
+This script just takes 2 files, and filters the incoming one into purely unique
+lines in the outgoing one.
+"""
+
+import os
+import re
+import sys
+import time
+
+
+def strip_duplicate(infile, outfile, insize=None):
+    from memory_dump import _intset
+    seen = _intset.IntSet()
+
+    address_re = re.compile(
+        r'{"address": (?P<address>\d+)'
+        )
+    if insize is not None:
+        in_mb = ' / %5.1f MiB' % (insize / 1024. / 1024,)
+    else:
+        in_mb = ''
+    bytes_read = 0
+    last_bytes = 0
+    lines_out = 0
+    tstart = time.time()
+    tlast = time.time()
+    for line_num, line in enumerate(infile):
+        bytes_read += len(line)
+        m = address_re.match(line)
+        if m:
+            address = int(m.group('address'))
+            if address not in seen:
+                outfile.write(line)
+                lines_out += 1
+                seen.add(address)
+        tnow = time.time()
+        if tnow - tlast > 0.2:
+            tlast = tnow
+            mb_read = bytes_read / 1024. / 1024
+            tdelta = tnow - tstart
+            sys.stderr.write(
+                'loading... line %d, %d out, %5.1f%s read in %.1fs\r'
+                % (line_num, lines_out, mb_read, in_mb, tdelta))
+
+
+def main(args):
+    import optparse
+    p = optparse.OptionParser(
+        '%prog [INFILE [OUTFILE]]')
+
+    opts, args = p.parse_args(args)
+
+    if len(args) > 2:
+        sys.stderr.write('We only support 2 filenames, not %d\n' % (len(args),))
+        return -1
+    if len(args) == 0:
+        infile = sys.stdin
+        insize = None
+        outfile = sys.stdout
+    else:
+        infile = open(args[0], 'rb')
+        insize = os.fstat(infile.fileno()).st_size
+        if len(args) == 1:
+            outfile = sys.stdout
+        else:
+            outfile = open(args[1], 'wb')
+    strip_duplicate(infile, outfile, insize)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
