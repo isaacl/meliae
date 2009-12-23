5f096bf3894b2d559234a3c6bc7a0499d82bdc48
John Arbash Meinel <john@arbash-meinel.com>
2009-12-23 09:14:24 -0600

    Release Meliae 0.1.0-final

--- meliae/__init__.py
+++ meliae/__init__.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2009 Canonical Ltd
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3 as
 # published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """A simple way to dump memory consumption of a running python program."""
 
-version_info = (0, 0, 1, 'dev', 0)
+version_info = (0, 1, 0, 'final', 0)
 __version__ = '.'.join(map(str, version_info))
 
